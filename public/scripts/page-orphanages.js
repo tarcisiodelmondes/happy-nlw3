16144e30afb11332aabc89a72d6995719d3d3073
Tarcisio Delmondes <tarcisiodelmondes@gmail.com>
2020-10-15 18:26:50 -0300

    finalizando o back-end

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,22 +1,35 @@
-const map = L.map("mapid").setView([51.505, -0.09], 13);
+const map = L.map("mapid").setView([-27.2109325, -49.6448719], 15);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-// Create icon
 const icon = L.icon({
-  iconUrl: "./public/images/map-marker.svg",
+  iconUrl: "./images/map-marker.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
   popupAnchor: [170, 2],
 });
-// Create popup
-const popup = L.popup({
-  closeButton: false,
-  className: "map-popup",
-  minWidth: 240,
-  minHeight: 240,
-}).setContent(
-  'Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"></a>'
-);
 
-L.marker([51.5, -0.09], { icon }).addTo(map).bindPopup(popup);
+function addMarker({ name, id, lat, lng } = orphanage) {
+  // Create popup
+  const popup = L.popup({
+    closeButton: false,
+    className: "map-popup",
+    minWidth: 240,
+    minHeight: 240,
+  }).setContent(
+    `${name} <a href="/orphanage?id=${id}"><img src="./images/arrow-white.svg"></a>`
+  );
+
+  L.marker([lat, lng], { icon }).addTo(map).bindPopup(popup);
+}
+
+const orphanagesSpan = document.querySelectorAll(".orphanages span");
+orphanagesSpan.forEach((span) => {
+  const orphanage = {
+    name: span.dataset.name,
+    id: span.dataset.id,
+    lat: span.dataset.lat,
+    lng: span.dataset.lng,
+  };
+  addMarker(orphanage);
+});
